826393023b1800f50cb771e6a14cbe1b6d3d5503
hirokuma <hiro9ma@gmail.com>
2026-05-06 22:59:50 +0900

    backend: local regtest electrum

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-// original: https://github.com/bitcoindevkit/book-of-bdk/blob/474aa61ecd444833354e4e1177000d90362f1471/examples/rust/syncing/electrum/src/main.rs
-use bdk_electrum::electrum_client::Client;
-use bdk_electrum::{electrum_client, BdkElectrumClient};
-use bdk_wallet::bitcoin::Network;
-use bdk_wallet::chain::spk_client::{SyncRequest, SyncRequestBuilder};
-use bdk_wallet::AddressInfo;
-use bdk_wallet::KeychainKind;
-use bdk_wallet::Wallet;
+use bdk_electrum::{BdkElectrumClient, electrum_client, electrum_client::Client};
+use bdk_wallet::{
+    AddressInfo, KeychainKind, Wallet,
+    bitcoin::Network,
+    chain::spk_client::{SyncRequest, SyncRequestBuilder},
+};
 
+const ELECTRUM_SERVER: &str = "tcp://localhost:50001";
 const STOP_GAP: usize = 50;
 const BATCH_SIZE: usize = 5;
 const EXTERNAL_DESCRIPTOR: &str = "tr(tprv8ZgxMBicQKsPdrjwWCyXqqJ4YqcyG4DmKtjjsRt29v1PtD3r3PuFJAjWytzcvSTKnZAGAkPSmnrdnuHWxCAwy3i1iPhrtKAfXRH7dVCNGp6/86'/1'/0'/0/*)#g9xn7wf9";
@@ -14,9 +13,9 @@ const INTERNAL_DESCRIPTOR: &str = "tr(tprv8ZgxMBicQKsPdrjwWCyXqqJ4YqcyG4DmKtjjsR
 
 fn main() {
     let mut wallet: Wallet = Wallet::create(EXTERNAL_DESCRIPTOR, INTERNAL_DESCRIPTOR)
-        .network(Network::Signet)
+        .network(Network::Regtest)
         .create_wallet_no_persist()
-        .unwrap();
+        .expect("Failed to create wallet");
 
     let address: AddressInfo = wallet.reveal_next_address(KeychainKind::External);
     println!(
@@ -25,8 +24,9 @@ fn main() {
     );
 
     // Create the Electrum client
-    let client: BdkElectrumClient<Client> =
-        BdkElectrumClient::new(electrum_client::Client::new("ssl://mempool.space:60602").unwrap());
+    let client: BdkElectrumClient<Client> = BdkElectrumClient::new(
+        electrum_client::Client::new(ELECTRUM_SERVER).expect("Failed to create Electrum client"),
+    );
 
     // Perform the initial full scan on the wallet
     println!("full_scanning...");
@@ -34,24 +34,28 @@ fn main() {
     let full_scan_request = wallet.start_full_scan();
     let update = client
         .full_scan(full_scan_request, STOP_GAP, BATCH_SIZE, true)
-        .unwrap();
+        .expect("Failed to perform full scan");
 
-    wallet.apply_update(update).unwrap();
+    wallet.apply_update(update).expect("Failed to apply update");
     let duration = start.elapsed();
     println!("full_scan elapsed: {:?}", duration);
-
-    let balance = wallet.balance();
-    println!("Wallet balance: {} sat", balance.total().to_sat());
 
     loop {
         std::thread::sleep(std::time::Duration::from_secs(10));
 
         let start = std::time::Instant::now();
         let sync_request = sync_request(&wallet);
-        let sync_response = client.sync(sync_request, BATCH_SIZE, false).unwrap();
-        wallet.apply_update(sync_response).unwrap();
+        let sync_response = client
+            .sync(sync_request, BATCH_SIZE, false)
+            .expect("Failed to sync");
+        wallet
+            .apply_update(sync_response)
+            .expect("Failed to apply update");
         let duration = start.elapsed();
         println!("sync elapsed: {:?}", duration);
+
+        let balance = wallet.balance();
+        println!("Wallet balance: {} sat\n", balance.total().to_sat());
     }
 }
 
@@ -71,10 +75,7 @@ fn sync_request(wallet: &Wallet) -> SyncRequestBuilder<(bdk_wallet::KeychainKind
     for tx in wallet.transactions() {
         if tx.chain_position.is_unconfirmed() {
             for out in &tx.tx_node.tx.output {
-                if let Some(index) = wallet
-                    .spk_index()
-                    .index_of_spk(out.script_pubkey.clone())
-                {
+                if let Some(index) = wallet.spk_index().index_of_spk(out.script_pubkey.clone()) {
                     spks_to_sync.insert((*index, out.script_pubkey.clone()));
                 }
             }
@@ -84,5 +85,4 @@ fn sync_request(wallet: &Wallet) -> SyncRequestBuilder<(bdk_wallet::KeychainKind
     SyncRequest::builder()
         .chain_tip(chain_tip)
         .spks_with_indexes(spks_to_sync)
-
 }
